f6d1569121a92a832b068d913d5d0e73effe82dd
krtuhin <tuhin.km2774@gmail.com>
2023-08-02 20:49:11 +0530

    Iteration in Thymeleaf, Use of status variable

--- src/main/java/com/thymeleaf/controllers/MainController.java
+++ src/main/java/com/thymeleaf/controllers/MainController.java
@@ -1,7 +1,10 @@
 package com.thymeleaf.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,5 +21,16 @@ public class MainController {
 
 		return "about";
 		// about.html
+	}
+
+	// example loop handler
+	@GetMapping("/loop")
+	public String thymeleafLoop(Model model) {
+
+		List<String> list = List.of("Tuhin", "Nayan", "Pujan", "Soumya");
+
+		model.addAttribute("list", list);
+
+		return "iterate";
 	}
 }
